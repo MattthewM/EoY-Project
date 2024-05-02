d35054d56e55d3f2e1a65715feb72067bc11f8bc
S1798783 <S1798783@S0085CG9168ZDV.student.HISD.ORG>
2024-05-02 14:48:56 -0500

    drawing the player onto the screen

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,12 @@
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage; 
 import java.awt.event.*; 
+
+
 public class Game  extends JPanel implements Runnable, KeyListener{
+
 	private ImageIcon background;
 	private BufferedImage back; 
 	private int key; 
@@ -13,71 +17,61 @@ public class Game  extends JPanel implements Runnable, KeyListener{
 	
 	
  
+
+
+
 	
 	public Game() {
-		new Thread(this).start();	
-		this.addKeyListener(this);
-		key =-1; 
-		screen='S';
-		music=new Sound();
-		playSound=true;
-		playSound2=true;
-		background = new ImageIcon("CheckerV3.png");
-		player = new PlayableGuy(100, 300, 50, 50, "Player.png");
-		
-		
-	
+	    new Thread(this).start();
+	    this.setFocusable(true); // Allow this JPanel to receive keyboard input
+	    this.addKeyListener(this);
+	    key = -1;
+	    screen = 'S';
+	    music = new Sound();
+	    playSound = true;
+	    playSound2 = true;
+	    background = new ImageIcon("CheckerV3.png");
+	    player = new PlayableGuy(100, 300, 100, 150, new ImageIcon ("Player.png")); // Adjust width and height as needed
 	}
-	
-public void screen(Graphics g2d) {
-	switch(screen) {
-		case 'S':
-			//start screen
-			g2d.drawImage(background.getImage(), 0, 0, getWidth(), getHeight(), this);
-            g2d.setColor(Color.red);
-            g2d.setFont(new Font("Arial", Font.BOLD, 24));
-            g2d.drawString("Press 'B' to begin the game", 200, 300);
-			drawStartScreen(g2d);
-			
-			break;
-		case 'G':
-			//game screen
 
-			g2d.drawImage(background.getImage(),0, 0, getWidth(), getHeight(), this);
 		
+		
+	
+	
 
-
-
-
-            
-            break;
-			 
-			 
-		case 'W':
-			g2d.drawImage(background.getImage(),0, 0, getWidth(), getHeight(), this);
-			 g2d.setColor(Color.red);
+	
+	public void screen(Graphics g2d) {
+	    switch(screen) {
+	        case 'S':
+	            // start screen
+	            g2d.drawImage(background.getImage(), 0, 0, getWidth(), getHeight(), this);
+	            g2d.setColor(Color.red);
+	            g2d.setFont(new Font("Arial", Font.BOLD, 24));
+	            g2d.drawString("Press 'B' to begin the game", 200, 300);
+	            drawStartScreen(g2d);
+	            break;
+	        case 'G':
+	            // game screen
+	            g2d.drawImage(background.getImage(),0, 0, getWidth(), getHeight(), this);
+	            drawPlayableGuy(g2d); // Draw the player
+	            break;
+	        case 'W':
+	            g2d.drawImage(background.getImage(),0, 0, getWidth(), getHeight(), this);
+	            g2d.setColor(Color.red);
 	            g2d.drawString("Congratulations! You won!", 200, 400);
-	            
-			//win screen
-	
-			
-			break;
-		case 'L':
-			g2d.drawImage(background.getImage(),0, 0, getWidth(), getHeight(), this);
-			g2d.setColor(Color.red);
-            g2d.drawString("Congratulations on losing ", 200, 400);
-			
-			//lose screen
-			
-			break;
-		}
-
+	            break;
+	        case 'L':
+	            g2d.drawImage(background.getImage(),0, 0, getWidth(), getHeight(), this);
+	            g2d.setColor(Color.red);
+	            g2d.drawString("Congratulations on losing ", 200, 400);
+	            break;
+	    }
 	}
 
+	public void drawPlayableGuy(Graphics g2d) {
+		g2d.drawImage(player.getPic().getImage(),player.getX(),player.getY(),player.getWidth(),player.getHeight(),this);
+	}
 
-public void drawPlayer(Graphics2D g2d) {
-    player.draw(g2d);
-}
 	public void run()
 	   {
 	   	try
@@ -98,19 +92,19 @@ public void drawPlayer(Graphics2D g2d) {
 		g2d.setFont( new Font("Broadway", Font.BOLD, 50));
 		g2d.setColor(Color.red);
 		g2d.drawString("Welcome to The Worlds Hardest Game", 200,400);
-		g2d.drawString("Use arrow keys to move", 200, 600);
-		
+		g2d.drawString("Press B to Begin", 200, 600);
+		g2d.drawString("Use arrow keys to move ", 200, 800);
 
 }
-
 	
-
+	
 	public void paint(Graphics g){
-
+		
 		Graphics2D twoDgraph = (Graphics2D) g; 
 		if( back ==null)
 			back=(BufferedImage)( (createImage(getWidth(), getHeight()))); 
 		
+
 		Graphics g2d = back.createGraphics();
 	
 		g2d.clearRect(0,0,getSize().width, getSize().height);
@@ -120,8 +114,15 @@ public void drawPlayer(Graphics2D g2d) {
 		g2d.drawString("key " + key, 340, 100);
 		screen(g2d);
 		twoDgraph.drawImage(back, null, 0, 0);
+
+
+
 	}
+
 	
+
+
+
 	//DO NOT DELETE
 	@Override
 	public void keyTyped(KeyEvent e) {
@@ -129,53 +130,53 @@ public void drawPlayer(Graphics2D g2d) {
 		
 		}		
 	
+
+
+
+
 //DO NOT DELETE
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		key= e.getKeyCode();{
-			System.out.println(key);
-			if(key==66) {
-				screen = 'G';
-			}
-				 
-		key= e.getKeyCode();
-		System.out.println(key);
-		if (key == 39) { 
-	        player.setdx(2);
+	    int key = e.getKeyCode();
+	    if (screen == 'S') { // Check if the start screen is active
+	        if (key == 66) { // Check if 'B' key is pressed
+	            screen = 'G'; // Switch to the game screen when 'B' is pressed
+	        }
+	    } else if (screen == 'G') { // Check if the game screen is active
+	        if (key == 39) { // Right arrow key
+	            player.setdx(2);
+	        } else if (key == 37) { // Left arrow key
+	            player.setdx(-2);
+	        } else if (key == 40) { // Down arrow key
+	            player.setdy(2);
+	        } else if (key == 38) { // Up arrow key
+	            player.setdy(-2);
+	        }
 	    }
-		else if (key == 37) { 
-	        player.setdx(-2);
-	    } 
-		else if (key == 40) { 
-	        player.setdy(2);
-	    } 
-		else if (key == 38) { 
-	        player.setdy(-2);
-	    }
-		
-		}
-		
 	}
-		
-	
-	
-		
-	
-	
-	
-	
-	
-	//DO NOT DELETE
+
 	@Override
 	public void keyReleased(KeyEvent e) {
-		
-		
-		
-		
+	    int key = e.getKeyCode();
+	    if (screen == 'G') { // Check if the game screen is active
+	        if (key == 39 || key == 37) { // Right or left arrow key
+	            player.setdx(0);
+	        } else if (key == 40 || key == 38) { // Down or up arrow key
+	            player.setdy(0);
+	        }
+	    }
 	}
+
+
+		
 	
 	
+public void move() {
 	
+	player.move();
 	
+	}
 }
+	
+
+	
--- src/PlayableGuy.java
+++ src/PlayableGuy.java
@@ -1,24 +1,47 @@
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import javax.swing.ImageIcon;
 
 public class PlayableGuy {
-    private int x;
-    private int y;
-    private int dx;
-    private int dy;
-    private int width;
-    private int height;
-    private ImageIcon imageIcon;
+    private int x, y, width, height;
+    public ImageIcon pic;
+    private int dx, dy;
+    
+    public PlayableGuy()
+    {
+    	x=0;
+    	y=0;
+    	width=100;
+    	height=100;
+    	pic = new ImageIcon("Player.png");
+    }
 
-    public PlayableGuy(int x, int y, int width, int height, String imagePath) {
+    
+
+    public PlayableGuy(int x, int y, int width, int height, ImageIcon p) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
-        this.imageIcon = new ImageIcon(imagePath);
+        pic=p;
     }
 
-    // Getters for dimensions and position
+    // Other methods...
+
+    public void move() {
+        x += dx;
+        y += dy;
+    }
+
+    public void setdx(int dx) {
+        this.dx = dx;
+    }
+
+    public void setdy(int dy) {
+        this.dy = dy;
+    }
+
     public int getX() {
         return x;
     }
@@ -34,23 +57,9 @@ public class PlayableGuy {
     public int getHeight() {
         return height;
     }
-
-    // Method to draw the playable guy
-    public void draw(Graphics2D g2d) {
-        g2d.drawImage(imageIcon.getImage(), x, y, width, height, null);
-    }
-
-    // Method to move the playable guy by a specified amount
-    public void setdx(int dx) {
-        this.dx = dx;
-    }
-
-    public void setdy(int dy) {
-        this.dy = dy;
-    }
-
-    public void move() {
-        x += dx;
-        y += dy;
-    }
+    public ImageIcon getPic()
+	{
+	return pic;
+	}
+   
 }
